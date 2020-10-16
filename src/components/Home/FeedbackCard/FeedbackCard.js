1df80f2b35f8e127990134d4555bd502892d7ba8
abuhorainhero <abuhorain2016@gmail.com>
2020-10-16 21:54:27 +0600

    home some added

--- src/components/Home/FeedbackCard/FeedbackCard.js
+++ src/components/Home/FeedbackCard/FeedbackCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './FeedbackCard.css';
 
 const FeedbackCard = ({ feedback }) => {
     return (
         <div className='col-md-4 col-sm-6 col-12 my-3'>
-            <div className="card p-3">
+            <div className="card p-3 feedback-card">
                 <div className="card-body d-flex justify-content-around">
                     <div>
                         <img style={{ height: '50px', width: '50px' }} src={feedback.img} alt="" />
--- src/components/Home/ServicesCard/ServicesCard.js
+++ src/components/Home/ServicesCard/ServicesCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ServicesCard.css';
 
 const ServicesCard = ({ services }) => {
 
     return (
         <div className='col-md-4 my-3'>
             <Link to={`/order/${services.title}`}>
-                <div className="card d-flex flex-column align-items-center py-5">
+                <div className="card services-card d-flex flex-column align-items-center py-5">
                     <img src={services.img} alt="" className="img-fluid" style={{ height: '50px', width: '50px' }} />
                     <div className="card-body">
                         <h4 className="card-title">{services.title}</h4>
